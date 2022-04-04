65da0def83ba79625af85e96daa94d6a9d4a52ba
Leo Alvarado <calvaradot1@est.ups.edu.ec>
2022-04-04 16:32:51 -0500

    New attribute for CIE11 class. New method for printing CIE11 content.

--- CIE11.py
+++ CIE11.py
@@ -1,10 +1,17 @@
 class CIE11:
 
+    diagnosis: str
     error: bool
     error_message: str
     chapters: list
 
-    def __init__(self, error: bool = False, error_message: str = '', chapters: list = []):
+    def __init__(self, error: bool = False, error_message: str = '', chapters=None):
+        if chapters is None:
+            chapters = []
         self.error = error
         self.error_message = error_message
         self.chapters = list(set(chapters))
+
+    def __str__(self):
+        return f'Diagnosis: {self.diagnosis}, Error: {self.error}, Error message: {self.error_message},' \
+               f' Chapters: {self.chapters}'
