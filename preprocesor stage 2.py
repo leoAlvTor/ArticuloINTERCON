accc03f9807fdeb8484f6f04e63c92a19150ab88
Leo Alvarado <calvaradot1@est.ups.edu.ec>
2022-04-04 16:34:22 -0500

    Getting CIE 11 chapters based on user typed diagnosis. Printing not found diagnosis.

--- preprocesor stage 2.py
+++ preprocesor stage 2.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import api_CIE11
+import warnings
+from tqdm import tqdm
+warnings.filterwarnings("default")
 
 
 def read_file(file_name: str, sheet: str) -> pd.DataFrame:
@@ -31,14 +34,24 @@ def join_lists_as_set(*args) -> set[str]:
     return set(final_list)
 
 
+def get_CIE11_diagnosis(diagnosis_list: set):
+    found_diagnosis = []
+    not_found_diagnosis = []
+
+    headers = api_CIE11.generate_headers()
+    for element in tqdm(diagnosis_list):
+        cie11_object = api_CIE11.search_diagnosis(api_CIE11.generate_body(element), headers)
+        cie11_object.diagnosis = element
+        found_diagnosis.append(cie11_object) if not cie11_object.error else not_found_diagnosis.append(cie11_object)
+    return found_diagnosis, not_found_diagnosis
+
+
 file = read_file(file_name="Datos pre-procesados 1196-2022.xlsx", sheet="Sheet1")
 unique_simple_values = get_unique_values(file, 'Diagnóstico')
 unique_separated_values = separate_string_values(unique_simple_values, separator='/')
 unique_simple_values = delete_values_by_condition(unique_simple_values, condition='/')
 
-final_diagnosis_list = join_lists_as_set(unique_simple_values, unique_separated_values)
-
-headers = api_CIE11.generate_headers()
-
-for element in final_diagnosis_list:
-    print(element, api_CIE11.search_diagnosis(api_CIE11.generate_body(element), headers).chapters)
+final_diagnosis_set = join_lists_as_set(unique_simple_values, unique_separated_values)
+found_diagnosis, not_found_diagnosis = get_CIE11_diagnosis(final_diagnosis_set)
+print('Not found diagnosis:', len(not_found_diagnosis))
+[print('Not found: ', x.__str__()) for x in not_found_diagnosis]
